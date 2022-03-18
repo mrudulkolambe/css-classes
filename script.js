7fd34932da06cbf6b5ab4c8c3a21d7fe023aba05
mrudulkolambe <mrudulkolambe02@gmail.com>
2022-03-18 22:25:17 +0530

    Tooltip Functionality

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const attribute_setting =  document.querySelectorAll("button");
 
 attribute_setting.forEach(attribute => {
-	attribute.setAttribute("data-name", attribute.firstChild.data || attribute.firstChild.textContent)
+	const data_value = attribute.firstChild.data || attribute.firstChild.textContent;
+	attribute.setAttribute("data-name", data_value)
+	if (attribute.classList.contains("btn-icon") && attribute.getAttribute('data-icon')) {
+		attribute.innerHTML = `<i class="${attribute.getAttribute('data-icon')}"></i>` +  attribute.innerHTML
+	}
 })
 
 
-const tooltip_container = document.querySelectorAll(".tooltip-container");
+const tooltip_container = document.querySelectorAll(".tooltip");
 
 tooltip_container.forEach(tooltip => {
-	tooltip.innerHTML += `<p class="tooltip">${tooltip.dataset.tooltip}</p>`
+	tooltip.innerHTML += `<p class="tooltip-container">${tooltip.dataset.tooltip}</p>`
+	let flag = "false";
+	try {
+		flag = tooltip.getAttribute('tooltip-animation')
+	} catch (error) {
+		return
+	}
+	if(flag === "true" && tooltip.classList.contains('tooltip')){
+		tooltip.querySelector(".tooltip-container").classList.add('tooltip_animation')
+	}
 })
