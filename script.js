fca6e6bd463ba4786ecee474c074024629862e6d
mrudulkolambe <mrudulkolambe02@gmail.com>
2022-03-18 23:44:56 +0530

    Bugs Fixed

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-const attribute_setting =  document.querySelectorAll("button");
+const attribute_setting = document.querySelectorAll("button");
 
-attribute_setting.forEach(attribute => {
-	const data_value = attribute.firstChild.data || attribute.firstChild.textContent;
-	attribute.setAttribute("data-name", data_value)
-	if (attribute.classList.contains("btn-icon") && attribute.getAttribute('data-icon')) {
-		attribute.innerHTML = `<i class="${attribute.getAttribute('data-icon')}"></i>` +  attribute.innerHTML
-	}
-})
+attribute_setting.forEach((attribute) => {
+  const data_value =
+    attribute.firstChild.data || attribute.firstChild.textContent;
+  attribute.setAttribute("data-name", data_value);
+});
 
+const icon_setting = document.querySelectorAll(".btn-icon");
+console.log(icon_setting)
+icon_setting.forEach((icon_setter) => {
+  if (
+    icon_setter.classList.contains("btn-icon") &&
+    icon_setter.getAttribute("data-icon")
+  ) {
+    icon_setter.innerHTML =
+      `<i class="${icon_setter.getAttribute("data-icon")}"></i>` +
+      icon_setter.innerHTML;
+  } else {
+    console.log("first");
+  }
+});
 
 const tooltip_container = document.querySelectorAll(".tooltip");
 
-tooltip_container.forEach(tooltip => {
-	tooltip.innerHTML += `<p class="tooltip-container">${tooltip.dataset.tooltip}</p>`
-	let flag = "false";
-	try {
-		flag = tooltip.getAttribute('tooltip-animation')
-	} catch (error) {
-		return
-	}
-	if(flag === "true" && tooltip.classList.contains('tooltip')){
-		tooltip.querySelector(".tooltip-container").classList.add('tooltip_animation')
-	}
-})
+tooltip_container.forEach((tooltip) => {
+  tooltip.innerHTML += `<p class="tooltip-container">${tooltip.dataset.tooltip}</p>`;
+  let flag = "false";
+  try {
+    flag = tooltip.getAttribute("tooltip-animation");
+  } catch (error) {
+    return;
+  }
+  if (flag === "true" && tooltip.classList.contains("tooltip")) {
+    tooltip
+      .querySelector(".tooltip-container")
+      .classList.add("tooltip_animation");
+  }
+});
